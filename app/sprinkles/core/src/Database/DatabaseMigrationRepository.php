26d2c9c95578834bfb52956330279bba3f3bed97
Louis Charette <charette.louis@gmail.com>
2017-12-02 20:52:33 -0500

    Added DatabaseMigrationRepository based on Laravel implementation

---
+++ app/sprinkles/core/src/Database/DatabaseMigrationRepository.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
+ */
+namespace UserFrosting\Sprinkle\Core\Database;
+
+use Illuminate\Database\ConnectionResolverInterface as Resolver;
+use Illuminate\Database\Migrations\DatabaseMigrationRepository as Repository;
+
+/**
+ * MigrationRepository Class
+ *
+ * Repository used to store all migrations run against the database
+ *
+ * @author Louis Charette
+ */
+class DatabaseMigrationRepository extends Repository
+{
+    /**
+     * Create the migration repository data store.
+     *
+     * @return void
+     */
+    public function createRepository()
+    {
+        $schema = $this->getConnection()->getSchemaBuilder();
+
+        $schema->create($this->table, function ($table) {
+            // The migrations table is responsible for keeping track of which of the
+            // migrations have actually run for the application. We'll create the
+            // table to hold the migration file's path as well as the batch ID.
+            $table->increments('id');
+            $table->string('sprinkle');
+            $table->string('migration');
+            $table->integer('batch');
+            $table->timestamps();
+        });
+    }
+}
