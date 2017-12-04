57967a11487c045814b8b42ebeb2263e906fc1bc
Louis Charette <charette.louis@gmail.com>
2017-12-03 21:32:09 -0500

    Moved the migration Bakery commands around

--- app/sprinkles/core/src/Bakery/MigrateCommand.php
+++ app/sprinkles/core/src/Bakery/MigrateCommand.php
@@ -5,14 +5,13 @@
  * @link      https://github.com/userfrosting/UserFrosting
  * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
  */
-namespace UserFrosting\System\Bakery\Command;
+namespace UserFrosting\Sprinkle\Core\Bakery;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use UserFrosting\System\Bakery\BaseCommand;
-use UserFrosting\System\Bakery\Migrator;
 
 /**
  * Migrate CLI Tools.
@@ -20,7 +19,7 @@ use UserFrosting\System\Bakery\Migrator;
  *
  * @author Alex Weissman (https://alexanderweissman.com)
  */
-class Migrate extends BaseCommand
+class MigrateCommand extends BaseCommand
 {
     /**
      * {@inheritDoc}
@@ -30,7 +29,8 @@ class Migrate extends BaseCommand
         $this->setName("migrate")
              ->setDescription("Perform database migration")
              ->setHelp("This command runs all the pending database migrations.")
-             ->addOption('pretend', 'p', InputOption::VALUE_NONE, 'Run migrations in "dry run" mode');
+             ->addOption('pretend', 'p', InputOption::VALUE_NONE, 'Run migrations in "dry run" mode')
+             ->addOption('database', 'd', InputOption::VALUE_REQUIRED, 'The database connection to use');
     }
 
     /**
@@ -40,9 +40,22 @@ class Migrate extends BaseCommand
     {
         $this->io->title("UserFrosting's Migrator");
 
+        // Get options
         $pretend = $input->getOption('pretend');
+        $database = $input->getOption('database');
 
-        $migrator = new Migrator($this->io, $this->ci);
-        $migrator->runUp($pretend);
+        /** @var UserFrosting\Sprinkle\Core\Database\Migrator */
+        $migrator = $this->ci->migrator;
+
+        // Set connection to the selected database
+        $migrator->setConnection($database);
+
+        if (! $this->migrator->repositoryExists()) {
+            $this->call(
+                'migrate:install', ['--database' => $this->option('database')]
+            );
+        }
+
+        $migrator->runUp($pretend, $database);
     }
 }
