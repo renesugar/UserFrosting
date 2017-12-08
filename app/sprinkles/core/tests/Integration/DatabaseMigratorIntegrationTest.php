748a39c1f630cd25f7067fd7cddea8ae4d430ee8
Louis Charette <charette.louis@gmail.com>
2017-12-08 16:04:40 -0500

    Added more tests

--- app/sprinkles/core/tests/Integration/DatabaseMigratorIntegrationTest.php
+++ app/sprinkles/core/tests/Integration/DatabaseMigratorIntegrationTest.php
@@ -106,37 +106,39 @@ class DatabaseMigratorIntegrationTest extends TestCase
         $this->assertEquals(array_reverse($this->locator->getMigrations()), $rolledBack);
     }
 
-    /*public function testMigrationsCanBeReset()
+    public function testMigrationsCanBeReset()
     {
-        $this->migrator->run([__DIR__.'/migrations/one']);
+        // Run up
+        $this->migrator->run();
         $this->assertTrue($this->schema->hasTable('users'));
         $this->assertTrue($this->schema->hasTable('password_resets'));
-        $rolledBack = $this->migrator->reset([__DIR__.'/migrations/one']);
+
+        $rolledBack = $this->migrator->reset();
         $this->assertFalse($this->schema->hasTable('users'));
         $this->assertFalse($this->schema->hasTable('password_resets'));
 
-        $this->assertTrue(Str::contains($rolledBack[0], 'password_resets'));
-        $this->assertTrue(Str::contains($rolledBack[1], 'users'));
+        // Make sure the data returned from migrator is accurate.
+        $this->assertEquals(array_reverse($this->locator->getMigrations()), $rolledBack);
     }
 
     public function testNoErrorIsThrownWhenNoOutstandingMigrationsExist()
     {
-        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->migrator->run();
         $this->assertTrue($this->schema->hasTable('users'));
         $this->assertTrue($this->schema->hasTable('password_resets'));
-        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->migrator->run();
     }
 
     public function testNoErrorIsThrownWhenNothingToRollback()
     {
-        $this->migrator->run([__DIR__.'/migrations/one']);
+        $this->migrator->run();
         $this->assertTrue($this->schema->hasTable('users'));
         $this->assertTrue($this->schema->hasTable('password_resets'));
-        $this->migrator->rollback([__DIR__.'/migrations/one']);
+        $this->migrator->rollback();
         $this->assertFalse($this->schema->hasTable('users'));
         $this->assertFalse($this->schema->hasTable('password_resets'));
-        $this->migrator->rollback([__DIR__.'/migrations/one']);
-    }*/
+        $this->migrator->rollback();
+    }
 
     /**
      *    !TODO :
